13363db61601903bfd71d607f48aa4c7cb319350
NayKayvd <vandiemennahum@gmail.com>
2021-07-20 16:29:28 +0200

    updated

--- app.py
+++ app.py
@@ -9,4 +9,9 @@ def get_chuck_norris_jokes():
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
 
-    return "<strong>Random joke from chuck norris: </strong>" + response['value']
+    return "<strong>Random joke from chuck norris: </strong><br>{}".format(response)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
